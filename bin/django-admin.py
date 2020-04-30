6fd70ccf30f4fc108ab7104e89f80e8e4de79e41
Dave Kaiser <davidwkaiser@yahoo.com>
2020-04-29 19:54:41 -0500

    add config for heroku and gitignore

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/I371030/Desktop/sarcasm_detector_python/sarcasm_detector_python_app/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
