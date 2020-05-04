439775e31a08a29c098ff70513ba54c403dd39f6
Dave Kaiser <davidwkaiser@yahoo.com>
2020-05-03 20:09:05 -0500

    add a view template

--- website/texts/views.py
+++ website/texts/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
-
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the texts index.")
+	template = loader.get_template('texts/index.html')
+	context = {}
+	return HttpResponse(template.render(context, request))	
+    # return render(request, 'texts/index.html', context)
