4908700dc279a5abf6269d5e556b0f8cf76a3029
Dave Kaiser <davidwkaiser@yahoo.com>
2020-05-06 11:26:03 -0500

    get post working in the form / urls / views

--- website/texts/views.py
+++ website/texts/views.py
@@ -10,4 +10,13 @@ def index(request):
 	template = loader.get_template('texts/index.html')
 	context = {}
 	return HttpResponse(template.render(context, request))	
-    # return render(request, 'texts/index.html', context)
+
+
+def show(request):
+	original_text = request.POST.get('inputtext')
+	template = loader.get_template('texts/show.html')
+	context = {
+        'original_text': original_text
+    }
+
+	return HttpResponse(template.render(context, request))	
